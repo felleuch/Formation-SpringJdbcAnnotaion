a8a7f282bc7e21d06f7aa8bad29f8379806883c0
ELLEUCH <ELLEUCH@DESKTOP-E28062K>
2020-04-08 14:06:02 +0200

    maj formation

--- src/main/java/com/technicalkeeda/bean/Formation.java
+++ src/main/java/com/technicalkeeda/bean/Formation.java
@@ -1,5 +1,38 @@
 package com.technicalkeeda.bean;
 
 public class Formation {
+	
+	int id;
+	
+	String name;
+	
+	String description;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
+	
 
 }
